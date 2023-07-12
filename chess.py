035bbebe103528cec8b00999ff2ce259ee8d2acb
Ken Wu <ken.wu@uq.net.au>
2023-07-12 21:15:20 +1000

    5e

--- chess.py
+++ chess.py
@@ -28,10 +28,16 @@ def pieces():
     cboard[7][4]="♚"
     for i in range (8):
         cboard[1][i]="♙"
-    for i in range (8):
+    for i in range (7):
         cboard[6][i]="♟︎"
     print_chessboard(cboard)
     return cboard
+
+def move_length(startcoord, endcoord):
+    #hor_move positive is movement towards the left, ver_move positive is up
+    ver_move = startcoord[0]-endcoord[0]
+    hor_move = startcoord[1]-endcoord[1]
+    return (ver_move, hor_move)
 
 def square_status(coord, cboard):
     #square status returns status of piece and false otherwise (empty)
@@ -39,16 +45,45 @@ def square_status(coord, cboard):
     piecetype = None
     if piece in chesspieces.keys():
         #print(f"The piece {piece} is a {chesspieces[piece]['Colour']} {chesspieces[piece]['Type']}")
-        piecetype = (chesspieces[piece]['Colour'],chesspieces[piece]['Type'])
+        piecetype = (chesspieces[piece]["Colour"],chesspieces[piece]["Type"])
+        #print(bool(piece))
         return piece
     else:
         #print("The square is empty")
         return False
     
-#def knight_move()
+def knight_move(startcoord, endcoord):
+    (hor_move,ver_move) = move_length(startcoord,endcoord)
+    if ((abs(hor_move) == 2 and abs(ver_move) == 1) or (abs(hor_move) == 1 and abs(ver_move) == 2)):
+        return True
+    else: 
+        return False
+
+def rook_move(startcoord, endcoord, cboard):
+    (ver_move,hor_move) = move_length(startcoord,endcoord)
+    #vertical movement, first down and then up
+    print(hor_move)
+    print(ver_move)
+    if hor_move == 0 and ver_move > 0:
+        for i in range(1, abs(ver_move)):
+            #if all in between are empty, is valid
+            print((startcoord[0]-i,startcoord[1]))
+            if square_status((startcoord[0]-i,startcoord[1]), cboard) == True:
+            
+                print(f"{(startcoord[0]-i,startcoord[1])} is nonempty")
+                return False
+            
+    else:
+        print("Not move")
+        return False
 
 def is_valid(originalpiece, turn, startcoord, endcoord, 
              capturestate, cboard, endsquare):
+    # Positions must be within [0:7]
+    if ((startcoord[0] or startcoord[1] or endcoord[0] or endcoord[1]) > 7 or 
+    (startcoord[0] or startcoord[1] or endcoord[0] or endcoord[1]) < 0):
+        return False
+    #Cannot go from same square to itself is satisfied by later conditions
     # If the start square is empty, invalid move
     if square_status(startcoord, cboard) == False:
         return False
@@ -57,7 +92,7 @@ def is_valid(originalpiece, turn, startcoord, endcoord,
         return False
     # If the end position is filled and no capture, invalid
     if (capturestate == False and 
-    square_status(endcoord, cboard) == True):
+    bool(square_status(endcoord, cboard)) == True):
         return False
     # If capturing and end position is empty, invalid
     if (capturestate == True and 
@@ -66,8 +101,12 @@ def is_valid(originalpiece, turn, startcoord, endcoord,
     # If capturing a same colour piece, invalid
     if (capturestate == True and chesspieces[endsquare]["Colour"] == turn):
         return False
-    
-
+    print(startcoord)
+    print(endcoord)
+    if chesspieces[originalpiece]["Type"] == "Knight":
+        return knight_move(startcoord, endcoord)
+    if chesspieces[originalpiece]["Type"] == "Rook":
+        return rook_move(startcoord, endcoord, cboard)
     return True    
 
 def checkmate():
