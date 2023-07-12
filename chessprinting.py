c19ef4ae6dbc915915e1739626252a4948f67869
Ken Wu <ken.wu@uq.net.au>
2023-07-12 13:10:21 +1000

    3e

--- chessprinting.py
+++ chessprinting.py
@@ -3,30 +3,33 @@ def chessboard():
     board = []
     for row in range(8):
         if row % 2 == 0:
-            for col in range(8):
+            for col in range(9):
                 if col % 2 == 0:
                     board += ["⬛"]
                 else:
                     board += ["⬜"]
         else:
             #board = []
-            for col in range(8):
+            for col in range(9):
                 if col % 2:
                     board += ["⬛"]
                 else:
                     board += ["⬜"]
-    for 
-    finalboard=[board[0:7],board[8:15],board[0:7],board[8:15],board[0:7],board[8:15],board[0:7],board[8:15]]
+    finalboard=[]
+    for _ in range(4):
+        finalboard += [board[0:8],board[9:17]]
     return finalboard
     
-board = chessboard()
+
 def print_chessboard(board):
-    print("  ", end= "")
+    print(" ", end= "\t")
     for i in range(len(board)):
-        print(i+1, end = " ")
+        print(chr(97+i), end = "\t")
     print()
     for i in range(len(board)):
-        print(i+1, end = " ")
+        #print("\n")
+        print(8-i, end = "\t")
         for j in range(len(board[i])):
-            print(board[i][j], end = "")
+            print(board[i][j], end = "\t")
         print()
+        
